e17c2a967accce7f23628291e7815ee1a30804f1
Adel Djahnit <adeldjahnita@gmail.com>
2023-10-13 11:18:08 +0200

    mise_en_place_sans_design

--- src/Composant/templates/HomePage.tsx
+++ src/Composant/templates/HomePage.tsx
@@ -1,18 +1,28 @@
+// src/components/templates/HomePage.tsx
 import React from 'react';
+import './HomePage.css'
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <header>
-        <h1>Mon Jeu de Société</h1>
-      </header>
-      <main>
-        <p>Bienvenue sur la page d'accueil de Mon Jeu de Société. Commencez à jouer dès maintenant !</p>
-        {/* Ajoutez ici des liens ou des éléments pour commencer le jeu */}
-      </main>
-      <footer>
-        <p>&copy; 2023 Mon Jeu de Société</p>
-      </footer>
+    <div className="home-page">
+      <div className="video-background">
+      <div className="content">
+        <div className="gif-background">
+        <img src="votre-gif.gif" alt="GIF en arrière-plan" />
+      </div>
+        <header>
+          <h1>Mon Jeu de Société</h1>
+        </header>
+        <main>
+          <p>Bienvenue sur la page d'accueil de Mon Jeu de Société. Commencez à jouer dès maintenant !</p>
+          <button className="play-button">Jouer</button>
+        </main>
+        <footer>
+          <button className="profile-button">Profil</button>
+          <p>&copy; 2023 Mon Jeu de Société</p>
+        </footer>
+      </div>
+      </div>
     </div>
   );
 };
