f8e7e9b5be35d12212dcd2dfd7dfe5281f70a054
Adel Djahnit <adeldjahnita@gmail.com>
2023-11-09 12:10:25 +0100

    img return

--- src/components/home/ReturnComponent.tsx
+++ src/components/home/ReturnComponent.tsx
@@ -2,16 +2,8 @@ import React from 'react';
 
 const ReturnComponent: React.FC = () => {
   return (
-    <div>
-      <div className="returnplay-button">
-        <div className="returngradient-border">
-          <div className="returngradient-border-inner">
-            <div className="returnplay-button-text">
-              Retour
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='returnAcceuil'>
+      <img src="https://cdn.discordapp.com/attachments/888154458528301097/1163232163274174555/Skull-King-logo.png?ex=653ed35f&is=652c5e5f&hm=cdfdaff777b271952802f8bdb79c3217a7e57d1333ae2cbc67d3c21a329c1697&" alt="Skull King Logo" />
     </div>
   );
 };
