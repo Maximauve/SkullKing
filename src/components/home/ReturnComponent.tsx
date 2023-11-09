abf37a2c54077df89273799c9a93983b9da65328
Adel Djahnit <adeldjahnita@gmail.com>
2023-11-09 11:30:43 +0100

    btn return

---
+++ src/components/home/ReturnComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ReturnComponent: React.FC = () => {
+  return (
+    <div>
+      <div className="returnplay-button">
+        <div className="returngradient-border">
+          <div className="returngradient-border-inner">
+            <div className="returnplay-button-text">
+              Retour
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReturnComponent;
