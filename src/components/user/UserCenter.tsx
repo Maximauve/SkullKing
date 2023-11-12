93739dd5300effcb64de8fce0ad001a80c8d9597
Kaly <sandra.heraud@ynov.com>
2023-11-12 23:32:48 +0100

    :truck: Move image to public

--- src/components/user/UserCenter.tsx
+++ src/components/user/UserCenter.tsx
@@ -15,7 +15,7 @@ const UserCenter = (): React.JSX.Element => {
   return (
     <>
       <div className="user-center">
-        <img className="user-profil-picture" src='https://cdn.discordapp.com/attachments/1159044537763766343/1172480833932775454/fotor-ai-2023111011201.jpg?ex=656078de&is=654e03de&hm=e226166a7bbe476d7991fd01d94b6998e4c0f2bff450a46d6389fa13694e0668&' alt="Profil Pic" />
+        <img className="user-profil-picture" src='/image/profil-picture.jpg' alt="Profil Pic" />
       </div>
       <button onClick={Logout}>
         Logout
