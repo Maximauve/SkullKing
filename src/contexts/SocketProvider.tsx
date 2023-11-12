e1f398248c9a03f0ee3e17dcff48f92b38002257
Maximauve <max.mourgues@gmail.com>
2023-11-12 21:41:57 +0100

    :sparkles: Clean && new env variable for ws

--- src/contexts/SocketProvider.tsx
+++ src/contexts/SocketProvider.tsx
@@ -1,12 +1,10 @@
-import React, { type Dispatch, type PropsWithChildren, /* useContext, */ useEffect, useReducer, useState } from 'react';
+import React, { type Dispatch, type PropsWithChildren, useEffect, useReducer, useState } from 'react';
 import { initialSocketState, SocketActionType, SocketReducer, type SocketState } from 'contexts/socketReducer';
 import { type Action } from 'types/Action';
 import { io } from 'socket.io-client';
 import { useNavigate, useParams } from 'react-router-dom';
-// import { type User } from '../types/user/User';
-// import { UserContext } from './UserProvider';
-import { type User } from '../types/user/User';
-import LoginRegisterModal from '../components/user/LoginRegisterModal';
+import { type User } from 'types/user/User';
+import LoginRegisterModal from 'components/user/LoginRegisterModal';
 
 export const SocketContext = React.createContext<[SocketState, Dispatch<Action<SocketActionType>>]>([
   initialSocketState,
@@ -14,11 +12,10 @@ export const SocketContext = React.createContext<[SocketState, Dispatch<Action<S
 ]);
 
 const initializeState = (user: User | undefined): SocketState => {
-  // const [{ user }] = useContext(UserContext);
   if (user !== undefined) {
     console.log('[SocketProvider] User is defined : ', user);
     const state: SocketState = {
-      socket: io(process.env.REACT_APP_API_BASE_URL as string, { query: { token: user.access_token } }),
+      socket: io(process.env.REACT_APP_WS_URL as string, { query: { token: user.access_token } }),
       loading: false
     };
     console.log('[SocketProvider] initializeState : ', state);
@@ -35,7 +32,6 @@ const SocketProvider: React.FC<PropsWithChildren<props>> = ({ user, children }) 
   const { socket } = state;
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
-  // const [{ user }] = useContext(UserContext);
   const [isLogged] = useState<boolean>(user !== undefined);
 
   useEffect(() => {
@@ -45,7 +41,7 @@ const SocketProvider: React.FC<PropsWithChildren<props>> = ({ user, children }) 
     console.log('[SocketProvider] socket : ', socket);
 
     if (socket === undefined && isLogged) {
-      const newSocket = io(process.env.REACT_APP_API_BASE_URL as string, { query: { token: (user as User).access_token } });
+      const newSocket = io(process.env.REACT_APP_WS_URL as string, { query: { token: (user as User).access_token } });
       console.log('[SocketProvider] newSocket : ', newSocket);
       dispatch({ type: SocketActionType.SET_SOCKET, payload: newSocket });
       console.log('[SocketProvider] socket should now be defined : ', socket);
