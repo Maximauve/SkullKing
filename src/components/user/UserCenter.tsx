762c8cb0303ba813eb5171ece9b99183e68f374d
Kaly <sandra.heraud@ynov.com>
2023-11-13 22:09:22 +0100

    :lipstick: User Center Done

--- src/components/user/UserCenter.tsx
+++ src/components/user/UserCenter.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useUser from 'hooks/useUser';
 
 const UserCenter = (): React.JSX.Element => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
   const { logOut } = useUser();
 
@@ -12,14 +13,20 @@ const UserCenter = (): React.JSX.Element => {
     logOut();
   };
 
+  const handleClick = (): void => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
       <div className="user-center">
-        <img className="user-profil-picture" src='/image/profil-picture.jpg' alt="Profil Pic" />
+        <img className="user-profil-picture" onClick={handleClick} src='/image/profil-picture.jpg' alt="Profil Pic" />
       </div>
-      <button onClick={Logout}>
-        Logout
-      </button>
+      <div className={`user-center-deploy ${isModalOpen ? '' : 'hidden'}`}>
+        <button onClick={Logout}>
+          Logout
+        </button>
+      </div>
     </>
   );
 };
